93cb721755fd78440ecd347defaa139b5195025b
rhinnyglory <rhinny.glory@credencys.com>
2018-08-16 18:39:38 +0530

    updated with staging url

--- dist/main.bundle.js
+++ dist/main.bundle.js
@@ -5077,7 +5077,8 @@ var BootstrapAlert = /** @class */ (function () {
 /* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "a", function() { return environment; });
 var environment = {
     production: false,
-    url: 'http://dev.credencys.com:3038/'
+    // url: 'http://dev.credencys.com:3038/'
+    url: 'http://64.50.160.68:7575/'
 };
 //# sourceMappingURL=environment.js.map
 
--- src/environments/environment.stage.ts
+++ src/environments/environment.stage.ts
@@ -1,4 +1,5 @@
 export const environment = {
     production: false,
-    url: 'http://dev.credencys.com:3038/'
+    // url: 'http://dev.credencys.com:3038/'
+    url: 'http://64.50.160.68:7575/'
   };
